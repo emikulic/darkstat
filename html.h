363479cb77804cf598138e56d94da49bae290783
Emil Mikulic <emikulic@gmail.com>
2011-06-04 18:21:43 +1000

    Remove unnecessary header sentinel.

--- html.h
+++ html.h
@@ -4,12 +4,11 @@
  * copyright (c) 2006 Ben Stewart.
  * copyright (c) 2010 Malte S. Stretz.
  */
-#ifndef __DARKSTAT_HTML_H
-#define __DARKSTAT_HTML_H
+
+struct str;
 
 void html_open(struct str *buf, const char *title, const int path_depth,
     const int want_graph_js);
 void html_close(struct str *buf);
 
-#endif
 /* vim:set ts=3 sw=3 tw=78 expandtab: */
