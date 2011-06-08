4ba869f67c6d34b24ae6cd63facab4669bc63ea4
Malte S. Stretz <mss@apache.org>
2011-06-08 14:33:59 +0200

    Add license blurb to darkproxy.php.
    Feel free to do whatever you like with this trivial code.

--- contrib/darkproxy.php
+++ contrib/darkproxy.php
@@ -4,6 +4,11 @@
 # This is a simple proxy script to test the proper implementation of
 # relative hrefs in combination with eg. lighttpd 1.4 which doesn't
 # support the features of mod_proxy_core yet.
+#
+# You may use, modify and redistribute this file under the terms of the
+# GNU General Public License version 2. (see COPYING.GPL)
+# Feel free to relicense this code under any Open Source License
+# approved by the Open Source Initiative.
 
 $darkstat = "http://localhost:667";
 
