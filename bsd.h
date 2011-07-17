f70ff4550af48cc5df237d78b31b3d38901a5d4b
Emil Mikulic <emikulic@gmail.com>
2011-07-17 15:37:03 +1000

    Simplify HAVE_ vs HAVE_REAL_

--- bsd.h
+++ bsd.h
@@ -8,20 +8,16 @@
 
 #include "config.h"
 
-#ifndef HAVE_REAL_STRLCPY
+#ifndef HAVE_STRLCPY
 size_t strlcpy(char *dst, const char *src, size_t siz);
 #endif
 
-#ifndef HAVE_REAL_STRLCAT
+#ifndef HAVE_STRLCAT
 size_t strlcat(char *dst, const char *src, size_t siz);
 #endif
 
-#ifndef HAVE_REAL_SETPROCTITLE
-#ifdef HAVE_SETPROCTITLE
-void setproctitle(const char *fmt, ...);
-#else
-#define setproctitle(fmt, ...)
-#endif
+#ifndef HAVE_SETPROCTITLE
+#define setproctitle(fmt, ...) /* no-op */
 #endif
 
 /* vim:set ts=3 sw=3 tw=78 expandtab: */
