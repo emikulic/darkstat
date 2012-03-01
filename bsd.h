b27a12e65d6ad5e8bd7ee1e43addd8ed9fc028c6
Emil Mikulic <emikulic@gmail.com>
2012-03-01 20:19:49 +1100

    Include bsd/{string,unistd}.h if present.

--- bsd.h
+++ bsd.h
@@ -5,8 +5,13 @@
  */
 
 #include <sys/types.h>
-
 #include "config.h"
+#ifdef HAVE_BSD_STRING_H
+# include <bsd/string.h>
+#endif
+#ifdef HAVE_BSD_UNISTD_H
+# include <bsd/unistd.h>
+#endif
 
 #ifndef HAVE_STRLCPY
 size_t strlcpy(char *dst, const char *src, size_t siz);
