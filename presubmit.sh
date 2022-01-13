4aa254403a0442df141ad0f8c8f9e301f634ae08
Emil Mikulic <emikulic@gmail.com>
2022-01-13 18:43:54 +1100

    Add presubmit script.

---
+++ presubmit.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# darkstat 3
+# copyright (c) 2022 Emil Mikulic.
+#
+# Run ./presubmit.sh before committing code.
+
+say() {
+  echo ==\> "$@" >&2
+}
+
+run() {
+  say "$@"
+  "$@" || { say ERROR!; exit 1; }
+}
+
+run ./config.status
+run ./tidy_linktypes_list.sh
+run make depend
+run ./test_headers.sh
+run make clean
+run make check
